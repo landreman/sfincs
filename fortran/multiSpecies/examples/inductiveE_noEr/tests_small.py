afcd6f485615bac39321477b17e9377b906130ac
Matt Landreman <matt.landreman@gmail.com>
2015-01-05 16:59:12 -0500

    Added multispecies example with inductive E field

---
+++ fortran/multiSpecies/examples/inductiveE_noEr/tests_small.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# This python script checks the sfincsOutput.h5 file for an example to 
+# see if the results are close to expected values.  This script may be
+# run directly, and it is also called when "make test" is run from the
+# main SFINCS directory.
+
+execfile('../testsCommon.py')
+
+desiredTolerance = 0.001
+
+numFailures = 0
+
+species = 0
+
+exit(numFailures > 0)
