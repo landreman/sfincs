1a2b3171b17eff4357e5e968ed9e01771f83bb1e
Matt Landreman <matt.landreman@gmail.com>
2015-02-03 08:58:20 -0500

    Updated example namelists

--- fortran/nonlinear/examples/tokamak_1species_FPCollisions_noEr_withPhi1/tests_small.py
+++ fortran/nonlinear/examples/tokamak_1species_FPCollisions_noEr_withPhi1/tests_small.py
@@ -12,6 +12,17 @@ desiredTolerance = 0.001
 numFailures = 0
 
 numFailures += shouldBe("FSABFlow[0,0;;;]", 3.31053540633345494E-002, desiredTolerance)
-numFailures += shouldBe("heatFlux_vd1_psiHat[0,0;;;]", 9.80304762108666200E-008, desiredTolerance)
+
+numFailures += shouldBe("particleFlux_vE0_psiHat[0,0;;;]", -2.24654e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vE_psiHat[0,0;;;]",  -2.24654e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vm_psiHat[0,0;;;]",   2.35599e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vd1_psiHat[0,0;;;]",  1.09442e-08, desiredTolerance)
+numFailures += shouldBe("particleFlux_vd_psiHat[0,0;;;]",   1.09442e-08, desiredTolerance)
+
+numFailures += shouldBe("heatFlux_vE0_psiHat[0,0;;;]", -8.42454e-08, desiredTolerance)
+numFailures += shouldBe("heatFlux_vE_psiHat[0,0;;;]",  -8.40097e-08, desiredTolerance)
+numFailures += shouldBe("heatFlux_vm_psiHat[0,0;;;]",   2.45283e-07, desiredTolerance)
+numFailures += shouldBe("heatFlux_vd1_psiHat[0,0;;;]",  1.61037e-07, desiredTolerance)
+numFailures += shouldBe("heatFlux_vd_psiHat[0,0;;;]",   1.61273e-07, desiredTolerance)
 
 exit(numFailures > 0)
--- fortran/nonlinear/examples/tokamak_1species_PASCollisions_noEr_withPhi1/tests_small.py
+++ fortran/nonlinear/examples/tokamak_1species_PASCollisions_noEr_withPhi1/tests_small.py
@@ -12,7 +12,15 @@ desiredTolerance = 0.001
 numFailures = 0
 
 numFailures += shouldBe("FSABFlow[0,0;;;]", 1.31689894494766199E-002, desiredTolerance)
-numFailures += shouldBe("particleFlux_vd1_psiHat[0,0;;;]", 2.43400486962836789E-007, desiredTolerance)
-numFailures += shouldBe("heatFlux_vd1_psiHat[0,0;;;]", 1.78992270500802452E-007, desiredTolerance)
+numFailures += shouldBe("particleFlux_vE0_psiHat[0,0;;;]", 1.82868e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vE_psiHat[0,0;;;]",  1.82868e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vm_psiHat[0,0;;;]",  5.27685e-08, desiredTolerance)
+numFailures += shouldBe("particleFlux_vd1_psiHat[0,0;;;]", 2.35637e-07, desiredTolerance)
+numFailures += shouldBe("particleFlux_vd_psiHat[0,0;;;]",  2.35637e-07, desiredTolerance)
+numFailures += shouldBe("heatFlux_vE0_psiHat[0,0;;;]", 6.85756e-08, desiredTolerance)
+numFailures += shouldBe("heatFlux_vE_psiHat[0,0;;;]",  6.84547e-08, desiredTolerance)
+numFailures += shouldBe("heatFlux_vm_psiHat[0,0;;;]",  5.98473e-08, desiredTolerance)
+numFailures += shouldBe("heatFlux_vd_psiHat[0,0;;;]",  1.28302e-07, desiredTolerance)
+numFailures += shouldBe("heatFlux_vd1_psiHat[0,0;;;]", 1.28423e-07, desiredTolerance)
 
 exit(numFailures > 0)
