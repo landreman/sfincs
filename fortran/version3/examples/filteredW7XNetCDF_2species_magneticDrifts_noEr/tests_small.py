30ac0c49176a7d50a714617a0ef59e877936fcb2
Matt Landreman <matt.landreman@gmail.com>
2015-07-27 19:48:48 +0200

    Added examples with magnetic drifts

---
+++ fortran/version3/examples/filteredW7XNetCDF_2species_magneticDrifts_noEr/tests_small.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# This python script checks the sfincsOutput.h5 file for an example to 
+# see if the results are close to expected values.  This script may be
+# run directly, and it is also called when "make test" is run from the
+# main SFINCS directory.
+
+execfile('../testsCommon.py')
+
+desiredTolerance = 0.001
+
+numFailures = 0
+
+# Species 1
+numFailures += shouldBe("FSABFlow[0,0;;;]", -3.829380699567663E-003, desiredTolerance)
+numFailures += shouldBe("particleFlux_vm_psiHat[0,0;;;]", -3.046431720535173E-007, desiredTolerance)
+numFailures += shouldBe("heatFlux_vm_psiHat[0,0;;;]", -1.462349694439989E-006, desiredTolerance)
+
+# Species 2
+numFailures += shouldBe("FSABFlow[1,0;;;]", -3.594147540383717E-004, desiredTolerance)
+numFailures += shouldBe("particleFlux_vm_psiHat[1,0;;;]", -6.257597194755813E-009, desiredTolerance)
+numFailures += shouldBe("heatFlux_vm_psiHat[1,0;;;]", -3.411992535031735E-008, desiredTolerance)
+
+exit(numFailures > 0)
