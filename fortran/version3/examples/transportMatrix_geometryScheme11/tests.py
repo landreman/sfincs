e5b0cf18e690e9346f686eed7cc6d3c67ee1b595
amollen <mollen@nephy.chalmers.se>
2015-10-22 11:12:25 +0200

    Changed the check for correct output in one of the examples

--- fortran/version3/examples/transportMatrix_geometryScheme11/tests.py
+++ fortran/version3/examples/transportMatrix_geometryScheme11/tests.py
@@ -11,20 +11,20 @@ desiredTolerance = 0.001
 
 numFailures = 0
 
-numFailures += shouldBe("transportMatrix[0,0;;;]", 0.000630586, desiredTolerance)
-numFailures += shouldBe("transportMatrix[0,1;;;]", 0.00199776, desiredTolerance)
+numFailures += shouldBe("transportMatrix[0,0;;;]", -0.000630586, desiredTolerance)
+numFailures += shouldBe("transportMatrix[0,1;;;]", -0.00199776, desiredTolerance)
 
 # This next line changed sign with Hakan's sign corrections:
 numFailures += shouldBe("transportMatrix[0,2;;;]", -0.0620745, desiredTolerance)
 
-numFailures += shouldBe("transportMatrix[1,0;;;]", 0.00199966, desiredTolerance)
-numFailures += shouldBe("transportMatrix[1,1;;;]", 0.0107542, desiredTolerance)
+numFailures += shouldBe("transportMatrix[1,0;;;]", -0.00199966, desiredTolerance)
+numFailures += shouldBe("transportMatrix[1,1;;;]", -0.0107542, desiredTolerance)
 
 # These next 3 lines changed sign with Hakan's sign corrections:
 numFailures += shouldBe("transportMatrix[1,2;;;]", -0.171768, desiredTolerance)
 numFailures += shouldBe("transportMatrix[2,0;;;]", -0.0621547, desiredTolerance)
 numFailures += shouldBe("transportMatrix[2,1;;;]", -0.172102, desiredTolerance)
 
-numFailures += shouldBe("transportMatrix[2,2;;;]", -35.0602, desiredTolerance)
+numFailures += shouldBe("transportMatrix[2,2;;;]", 35.0602, desiredTolerance)
 
 exit(numFailures > 0)
